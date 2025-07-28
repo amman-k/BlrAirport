b86a32f1413f4f4c288e84ebfc21285861a3f375
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-07-28 20:11:11 +0530

    added route for departure

--- server/routes/flightRoutes.js
+++ server/routes/flightRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getArrivals } = require('../controllers/flightController');
+const { getDepartures } = require('../controllers/flightController');
 
 router.get('/arrivals', getArrivals);
+router.get('/departures',getDepartures)
 
 module.exports = router;
