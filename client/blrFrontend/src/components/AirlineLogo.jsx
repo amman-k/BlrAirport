db947602179bc9b9c4193967abb74aa020857843
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-07-29 11:13:24 +0530

    added more airline logos

--- client/blrFrontend/src/components/AirlineLogo.jsx
+++ client/blrFrontend/src/components/AirlineLogo.jsx
@@ -5,8 +5,61 @@ const airlineDomains = {
   'Vistara': "airvistara.com",
   "Air India": "airindia.com",
   'SpiceJet': "spicejet.com",
-  "Akasa Air": "akasaair.com",
   "AirAsia India": "airasia.co.in",
+  "Air India Express": "airindiaexpress.com",
+  "AIX Connect": "aixconnect.in",
+  "Alliance Air": "allianceair.in",
+  "Qatar Airways": "qatarairways.com",
+  "Singapore Airlines": "singaporeair.com",
+  "Cathay Pacific Airways": "cathaypacific.com",
+  'Emirates': "emirates.com",
+  "ANA": "ana.co.jp",
+  "Turkish Airlines": "turkishairlines.com",
+  "Korean Air": "koreanair.com",
+  "Air France": "airfrance.com",
+  "Hainan Airlines": "hnair.com.cn",
+  "Swiss International Air Lines": "swiss.com",
+  "EVA Air": "evaair.com",
+  "British Airways": "britishairways.com",
+  "Qantas": "qantas.com",
+  'Lufthansa': "lufthansa.com",
+  "Virgin Atlantic": "virginatlantic.com",
+  'Saudia': "saudia.com",
+  "STARLUX Airlines": "starlux-airlines.com",
+  "Air Canada": "aircanada.com",
+  'Iberia': "iberia.com",
+  "KLM Royal Dutch Airlines": "klm.com",
+  "Delta Air Lines": "delta.com",
+  "Austrian Airlines": "austrian.com",
+  "Air New Zealand": "airnewzealand.com",
+  'Finnair': "finnair.com",
+  "Etihad Airways": "etihad.com",
+  "Malaysia Airlines": "malaysiaairlines.com",
+  'AirAsia': "airasia.com",
+  "Thai Airways": "thaiairways.com",
+  'Scoot': "flyscoot.com",
+  "Fiji Airways": "fijiairways.com",
+  "Bangkok Airways": "bangkokair.com",
+  "China Southern Airlines": "csair.com",
+  "Virgin Australia": "virginaustralia.com",
+  "Gulf Air": "gulfair.com",
+  "Air Astana": "airastana.com",
+  "China Airlines": "china-airlines.com",
+  "Ethiopian Airlines": "ethiopianairlines.com",
+  'Eurowings': "eurowings.com",
+  "Asiana Airlines": "flyasiana.com",
+  "Vueling Airlines": "vueling.com",
+  "LATAM Airlines": "latam.com",
+  "Porter Airlines": "porterairlines.com",
+  'Volotea': "volotea.com",
+  "Garuda Indonesia": "garuda-indonesia.com",
+  "Aegean Airlines": "aegeanair.com",
+  "Oman Air": "omanair.com",
+  "Transavia France": "transavia.com",
+  "Qantas Airways": "qantas.com",
+  "KLM": "klm.com",
+  "Cathay Pacific": "cathaypacific.com",
+  "SriLankan Airlines": "srilankan.com"
 };
 
 const AirlineLogo = ({ airlineName }) => {
@@ -31,7 +84,7 @@ const AirlineLogo = ({ airlineName }) => {
       alt={`${airlineName} logo`}
       className="w-8 h-8 rounded-full object-contain bg-white p-1"
       onError={(e) => {
-        e.currentTarget.style.display = "none"; 
+        e.currentTarget.style.display = "none";
         const fallback = document.createElement("div");
         fallback.innerHTML = `<div class="w-8 h-8 rounded-full bg-slate-700 flex items-center justify-center text-slate-400 font-bold">${airlineName?.charAt(
           0
