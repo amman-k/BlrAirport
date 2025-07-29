3bd6d1c4196038b2b70838bdecbb06015d9c52bf
Amman Kashyap <ammankashyap@Ammans-MacBook-Pro-2.local>
2025-07-29 10:50:34 +0530

    data for directory

---
+++ client/blrFrontend/src/data/directory.js
@@ -0,0 +1,10 @@
+export const directoryData = [
+  { name: 'Hatti Kaapi', category: 'Food', location: 'T2, Domestic Departures' },
+  { name: 'TATA Starbucks', category: 'Food', location: 'T2, International Departures' },
+  { name: '7-Eleven', category: 'Shopping', location: 'T1, Arrivals' },
+  { name: 'Duty Free Store', category: 'Shopping', location: 'T2, International Departures' },
+  { name: 'Currency Exchange', category: 'Services', location: 'T1 & T2, Arrivals' },
+  { name: 'La Tapenade', category: 'Food', location: 'T2, Domestic Departures' },
+  { name: 'Shoppers Stop', category: 'Shopping', location: 'T2, Domestic Departures' },
+  { name: 'Medical Centre', category: 'Services', location: 'T1, Arrivals' },
+];
